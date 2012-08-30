9fe357d26a96960829848dcfd61bb48895c7b4e6
Luis Cabellos <zhen.sydow@gmail.com>
2012-08-30 17:27:47 +0200

    better tabs

--- AdvTiledLoader/Loader.lua
+++ AdvTiledLoader/Loader.lua
@@ -40,31 +40,31 @@ local fullpath -- The full path to the tmx file minus the name
 
 -- Loads a Map from a tmx file and returns it.
 function Loader.load(tofile)
-	
+
 	-- Get the raw path
 	fullpath = Loader.path .. tofile
-	
+
 	-- Process directory up
 	while string.find(fullpath, "[/\\][^/^\\]+[/\\]%.%.[/\\]") do
 		fullpath = string.gsub(fullpath, "[/\\][^/^\\]+[/\\]%.%.[/\\]", "/", 1)
 	end
-	
+
 	-- Get the file name
 	filename = string.match(fullpath, "[^/^\\]+$")
 	-- Get the path to the file
 	fullpath = string.gsub(fullpath, "[^/^\\]+$", "")
-	
+
 	-- Read the file and parse it into a table
 	local t = love.filesystem.read(fullpath .. filename)
 	t = xml_to_table(t)
-	
+
 	-- Get the map and process it
 	for _, v in pairs(t) do
 		if v.label == "map" then
 			return Loader._processMap(fullpath .. filename, v)
 		end
 	end
-	
+
 	-- If we made this this far then there wasn't a map tag
 	error("Loader.load - No map found in file " .. fullpath .. filename)
 end
@@ -73,13 +73,13 @@ end
 -- Private
 ----------------------------------------------------------------------------------------------------
 
--- Returns a new image from the filename. If Loader.fixPO2 is set to true then 
+-- Returns a new image from the filename. If Loader.fixPO2 is set to true then
 -- the images will automatically be padded to the power of 2.
 function Loader._newImage(info)
 	local source, padded, image
-	
+
 	-- If the image information is image data then assign it as the source
-	if info:type() == "ImageData" then  
+	if info:type() == "ImageData" then
 		source = info
 	-- Otherwise turn it into image data first
 	else
@@ -87,24 +87,24 @@ function Loader._newImage(info)
 	end
 
     local w, h = source:getWidth(), source:getHeight()
-	
+
 	-- If we dont need to pad for PO2 then return now
 	if not Loader.fixPO2 then return love.graphics.newImage(source), w, h end
-	
+
     -- Find closest power-of-two.
     local wp = math.pow(2, math.ceil(math.log(w)/math.log(2)))
     local hp = math.pow(2, math.ceil(math.log(h)/math.log(2)))
-   
+
     -- Only pad if needed:
     if wp ~= w or hp ~= h then
-        padded = love.image._newImageData(wp, hp)
-        padded:paste(source, 0, 0)
+	padded = love.image._newImageData(wp, hp)
+	padded:paste(source, 0, 0)
 	else
 		padded = source
     end
-   
+
 	-- Return the fixed image
-    return love.graphics._newImage(padded), w, h 
+    return love.graphics._newImage(padded), w, h
 end
 
 -- Checks to see if the table is a valid XML table
@@ -122,7 +122,7 @@ end
 function Loader._checkName(t, str)
 	while t[str] do
 		if string.find(str, "%(%d+%)$") == nil then str = str .. "(1)" end
-		str = string.gsub(str, "%(%d+%)$", function(a) return "(" .. 
+		str = string.gsub(str, "%(%d+%)$", function(a) return "(" ..
 								tonumber( string.sub(a, string.find(a, "%d+")) ) + 1 .. ")" end)
 	end
 	return str
@@ -134,7 +134,7 @@ function Loader._processProperties(t)
 	-- Do some checking
 	Loader._checkXML(t)
 	assert(t.label == "properties", "Loader._processProperties - Passed value is not a properties table")
-	
+
 	-- Create a properties table and populate it. Will attempt to convert the property to a number.
 	local prop = {}
 	for _,v in pairs(t) do
@@ -143,65 +143,65 @@ function Loader._processProperties(t)
 			prop[v.xarg.name] = tonumber(v.xarg.value) or v.xarg.value
 		end
 	end
-	
+
 	-- Return the properties
 	return prop
 end
 
 -- Process Map data from xml table
 function Loader._processMap(name, t)
-	
-	-- Do some checking
-	Loader._checkXML(t)
-	assert(t.label == "map", "Loader._processMap - Passed table is not a map")
-	assert(t.xarg.width, t.xarg.height, t.xarg.tilewidth, t.xarg.tileheight,
-		   "Loader._processMap - Map data is corrupt")
 
-	-- We'll use these for temporary storage
-	local map, tileset, tilelayer, objectlayer
-	
-	-- Create the map from the settings
-	local map = Map:new(name, tonumber(t.xarg.width),tonumber(t.xarg.height), 
-						tonumber(t.xarg.tilewidth), tonumber(t.xarg.tileheight), 
-						t.xarg.orientation)
-							
-	-- Apply the loader settings
-	map.useSpriteBatch = Loader.useSpriteBatch
-	map.drawObjects = Loader.drawObjects
-	
-	-- Now we fill it with the content
-	for _, v in ipairs(t) do
-		
-		-- Process TileSet
-		if v.label == "tileset" then 
-			tileset = Loader._processTileSet(v, map)
-			map.tilesets[tileset.name] = tileset
-			map:updateTiles()
-		end
-			
-		-- Process TileLayer
-		if v.label == "layer" then
-			tilelayer = Loader._processTileLayer(v, map)
-			map.tl[tilelayer.name] = tilelayer
-			map.drawList[#map.drawList + 1] = tilelayer
-		end
-		
-		-- Process ObjectLayer
-		if v.label == "objectgroup" then
-			objectlayer = Loader._processObjectLayer(v, map)
-			map.ol[objectlayer.name] = objectlayer
-			map.drawList[#map.drawList + 1] = objectlayer
-		end
-		
-		-- Process Map properties
-		if v.label == "properties" then
-			map.properties = Loader._processProperties(v)
-		end
-			
-	end
-	
-	-- Return our map
-	return map
+   -- Do some checking
+   Loader._checkXML(t)
+   assert(t.label == "map", "Loader._processMap - Passed table is not a map")
+   assert(t.xarg.width, t.xarg.height, t.xarg.tilewidth, t.xarg.tileheight,
+	  "Loader._processMap - Map data is corrupt")
+
+   -- We'll use these for temporary storage
+   local map, tileset, tilelayer, objectlayer
+
+   -- Create the map from the settings
+   local map = Map:new(name, tonumber(t.xarg.width),tonumber(t.xarg.height),
+		       tonumber(t.xarg.tilewidth), tonumber(t.xarg.tileheight),
+		       t.xarg.orientation)
+
+   -- Apply the loader settings
+   map.useSpriteBatch = Loader.useSpriteBatch
+   map.drawObjects = Loader.drawObjects
+
+   -- Now we fill it with the content
+   for _, v in ipairs(t) do
+
+      -- Process TileSet
+      if v.label == "tileset" then
+	 tileset = Loader._processTileSet(v, map)
+	 map.tilesets[tileset.name] = tileset
+	 map:updateTiles()
+      end
+
+      -- Process TileLayer
+      if v.label == "layer" then
+	 tilelayer = Loader._processTileLayer(v, map)
+	 map.tl[tilelayer.name] = tilelayer
+	 map.drawList[#map.drawList + 1] = tilelayer
+      end
+
+      -- Process ObjectLayer
+      if v.label == "objectgroup" then
+	 objectlayer = Loader._processObjectLayer(v, map)
+	 map.ol[objectlayer.name] = objectlayer
+	 map.drawList[#map.drawList + 1] = objectlayer
+      end
+
+      -- Process Map properties
+      if v.label == "properties" then
+	 map.properties = Loader._processProperties(v)
+      end
+
+   end
+
+   -- Return our map
+   return map
 end
 
 -- Process TileSet from xml table
@@ -210,11 +210,11 @@ function Loader._processTileSet(t, map)
 	-- Do some checking
 	Loader._checkXML(t)
 	assert(t.label == "tileset", "Loader._processTileSet - Passed table is not a tileset")
-	
-	-- If the tileset is an external one then replace it as the tileset. The firstgid is 
-	-- stored in the tileset tag in the original file while the rest of the tileset information 
+
+	-- If the tileset is an external one then replace it as the tileset. The firstgid is
+	-- stored in the tileset tag in the original file while the rest of the tileset information
 	-- is stored in the external file.
-	if t.xarg.source then 
+	if t.xarg.source then
 		local gid = t.xarg.firstgid
 		t = love.filesystem.read(Loader.path .. t.xarg.source)
 		for _,v in pairs(xml_to_table(t)) do if v.label == "tileset" then t = v end end
@@ -222,16 +222,16 @@ function Loader._processTileSet(t, map)
 	end
 	assert(t.xarg.name and t.xarg.tilewidth and t.xarg.tileheight and t.xarg.firstgid,
 		   "Loader._processTileSet - Tileset data is corrupt")
-	
+
 	-- Temporary storage
 	local image, imageWidth, imageHeight, path, prop
 	local tileProperties = {}
 	local tileoffset = {x=0,y=0}
-	
+
 	-- Process elements
 	for _, v in ipairs(t) do
 		-- Process image
-		if v.label == "image" then 
+		if v.label == "image" then
 			path = fullpath .. v.xarg.source
 			-- Process directory up
 			while string.find(path, "[^/^\\]+[/\\]%.%.[/\\]") do
@@ -244,11 +244,11 @@ function Loader._processTileSet(t, map)
 				imageHeight = cache_imagesize[image].height
 			-- Else load it and store in the cache
 			else
-				image = love.image.newImageData(path) 
+				image = love.image.newImageData(path)
 				-- transparent color
 				if v.xarg.trans then
-					local trans = { tonumber( "0x" .. v.xarg.trans:sub(1,2) ), 
-									tonumber( "0x" .. v.xarg.trans:sub(3,4) ), 
+					local trans = { tonumber( "0x" .. v.xarg.trans:sub(1,2) ),
+									tonumber( "0x" .. v.xarg.trans:sub(3,4) ),
 									tonumber( "0x" .. v.xarg.trans:sub(5,6) )}
 					image:mapPixel( function(x,y,r,g,b,a)
 					return r,g,b, (trans[1] == r and trans[2] == g and trans[3] ==b and 0) or a  end
@@ -262,9 +262,9 @@ function Loader._processTileSet(t, map)
 				cache_imagesize[image] = {width = imageWidth, height = imageHeight}
 			end
 		end
-		
+
 		-- Process tile properties
-		if v.label == "tile" then 
+		if v.label == "tile" then
 			for _, v2 in ipairs(v) do
 				if v2.label == "properties" then
 					-- Store the property. We must increase the id the starting gid
@@ -272,27 +272,27 @@ function Loader._processTileSet(t, map)
 				end
 			end
 		end
-		
+
 		-- Process tile set properties
 		if v.label == "properties" then
 			local tileSetProperties = Loader._processProperties(v)
 		end
-		
+
 		-- Get the tile offset if there is one.
 		if v.label == "tileoffset" then
 			tileoffset.x, tileoffset.y = tonumber(v.xarg.x or 0), tonumber(v.xarg.y or 0)
 		end
 
 	end
-	
+
 	-- Make sure that an image was loaded
 	assert(image, "Loader._processTileSet - Tileset did not contain an image")
 
 	-- Return the TileSet
-	local tileset = TileSet:new(image, Loader._checkName(map.tilesets, t.xarg.name), 
+	local tileset = TileSet:new(image, Loader._checkName(map.tilesets, t.xarg.name),
 					   tonumber(t.xarg.tilewidth), tonumber(t.xarg.tileheight),
 					   tonumber(imageWidth), tonumber(imageHeight),
-					   tonumber(t.xarg.firstgid), tonumber(t.xarg.spacing), 
+					   tonumber(t.xarg.firstgid), tonumber(t.xarg.spacing),
 					   tonumber(t.xarg.margin), tileProperties, tileSetProperties)
 	tileset.tileoffset = tileoffset
 	return tileset
@@ -304,23 +304,23 @@ function Loader._processTileLayer(t, map)
 	-- Do some checking
 	Loader._checkXML(t)
 	assert(t.label == "layer", "Loader._processTileLayer - Passed table is not a tileset")
-	
+
 	-- Process elements
 	local data, properties
 	for _, v in ipairs(t) do
 		Loader._checkXML(t)
-		
+
 		-- Process data
-		if v.label == "data" then 
-			data = Loader._processTileLayerData(v) 
+		if v.label == "data" then
+			data = Loader._processTileLayerData(v)
 		end
-		
+
 		-- Process TileLayer properties
 		if v.label == "properties" then
 			properties = Loader._processProperties(v)
 		end
 	end
-	
+
 	-- Return the new layer
 	local tl = TileLayer:new(map, t.xarg.name, t.xarg.opacity, properties)
 	tl:_populate(data)
@@ -333,17 +333,17 @@ function Loader._processTileLayerData(t)
 	-- Do some checking
 	Loader._checkXML(t)
 	assert(t.label == "data", "Loader._processTileLayerData - Passed table is not TileLayer data")
-	
+
 	local data = {}
-	
+
 	-- If encoded by comma seperated value (csv) then cut each value out and put it into a table.
 	if t.xarg.encoding == "csv" then
-        	string.gsub(t[1], "[%-%d]+", function(a) data[#data+1] = tonumber(a) or 0 end)
+		string.gsub(t[1], "[%-%d]+", function(a) data[#data+1] = tonumber(a) or 0 end)
 	end
-	
+
 	-- Base64 encoding. See base64.lua for more details.
 	if t.xarg.encoding == "base64" then
-	
+
 		-- If a compression method is used
 		if t.xarg.compression == "gzip" or t.xarg.compression == "zlib"  then
 			-- Select the appropriate function
@@ -360,16 +360,16 @@ function Loader._processTileLayerData(t)
 			data = base64.decode("int", t[1])
 		end
 	end
-	
+
 	-- If there is no encoding then the file is probably saved as XML
 	if t.xarg.encoding == nil then
 		for k,v in ipairs(t) do
-			if v.label == "tile" then 
+			if v.label == "tile" then
 				data[#data+1] = tonumber(v.xarg.gid)
 			end
 		end
 	end
-	
+
 	-- Return the data
 	return data
 end
@@ -380,64 +380,64 @@ function Loader._processObjectLayer(t, map)
 	-- Do some checking
 	Loader._checkXML(t)
 	assert(t.label == "objectgroup", "Loader._processObjectLayer - Passed table is not ObjectLayer data")
-	
-	-- Tiled stores colors in hexidecimal format that looks like "#FFFFFF" 
+
+	-- Tiled stores colors in hexidecimal format that looks like "#FFFFFF"
 	-- We need go convert them into base 10 RGB format
 	if t.xarg.color == nil then t.xarg.color = "#000000" end
-	local color = { tonumber( "0x" .. t.xarg.color:sub(2,3) ), 
-					tonumber( "0x" .. t.xarg.color:sub(4,5) ), 
+	local color = { tonumber( "0x" .. t.xarg.color:sub(2,3) ),
+					tonumber( "0x" .. t.xarg.color:sub(4,5) ),
 					tonumber( "0x" .. t.xarg.color:sub(6,7) )}
-	
+
 	-- Create a new layer
-	local layer = ObjectLayer:new(map, Loader._checkName(map.ol, t.xarg.name), color, 
+	local layer = ObjectLayer:new(map, Loader._checkName(map.ol, t.xarg.name), color,
 								  t.xarg.opacity)
-					
+
 	-- Process elements
 	local objects = {}
 	local prop, obj, poly
 	for _, v in ipairs(t) do
-	
+
 		-- Process objects
 		local obj
 		if v.label == "object" then
-			obj = Object:new(layer, v.xarg.name, v.xarg.type, tonumber(v.xarg.x), 
-											 tonumber(v.xarg.y), tonumber(v.xarg.width), 
+			obj = Object:new(layer, v.xarg.name, v.xarg.type, tonumber(v.xarg.x),
+											 tonumber(v.xarg.y), tonumber(v.xarg.width),
 											 tonumber(v.xarg.height), tonumber(v.xarg.gid) )
 			objects[#objects+1] = obj
 			for _, v2 in ipairs(v) do
-			
+
 				-- Process object properties
-				if v2.label == "properties" then 
+				if v2.label == "properties" then
 					obj.properties = Loader._processProperties(v2)
 				end
-				
+
 				-- Process polyline objects
 				if v2.label == "polyline" then
 					obj.polyline = {}
 					string.gsub(v2.xarg.points, "[%-%d]+", function(a) obj.polyline[#obj.polyline+1] = tonumber(a) or 0 end)
 				end
-				
+
 				-- Process polyline objects
 				if v2.label == "polygon" then
 					obj.polygon = {}
 					string.gsub(v2.xarg.points, "[%-%d]+", function(a) obj.polygon[#obj.polygon+1] = tonumber(a) or 0 end)
 				end
-			
+
 			end
 			obj:updateDrawInfo()
 		end
-		
+
 		-- Process properties
 		if v.label == "properties" then
 			prop = Loader._processProperties(v)
 		end
-		
+
 	end
-	
+
 	-- Set the properties and object tables
 	layer.properties = prop
 	layer.objects = objects
-	
+
 	-- Return the layer
 	return layer
 end
@@ -469,4 +469,3 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.--]]
-
--- AdvTiledLoader/TileLayer.lua
+++ AdvTiledLoader/TileLayer.lua
@@ -13,40 +13,40 @@ TileLayer.__index = TileLayer
 
 -- Returns a new TileLayer
 function TileLayer:new(map, name, opacity, prop)
-	assert( map and name , "TileLayer:new - Needs at least 2 parameters for the map and name.")
-	local tl = {}
-	
-	-- Public:
-	tl.name = name				-- The name of the tile layer
-	tl.map = map 				-- The map that this layer belongs to
-	tl.opacity = opacity or 1	-- The opacity to draw the tiles (0-1)
-	tl.properties = prop or {}	-- Properties set by Tiled
-	tl.tileData = Grid:new()	-- A grid containing the tile locations
-	tl.useSpriteBatch = nil		-- If true then the layer is rendered with sprite batches. If
-									-- false then the layer will not use sprite batches. If nil then 
-									-- map.useSpriteBatch will be used. Note that using sprite 
-									-- batches will break the draw order when using multiple tilesets
-									-- on the same layer. Using Map.drawAfterTile is also not possible.
-	-- Private:
-	tl._batches = {}						-- Keeps track of the sprite batches for each tileset
-	tl._flippedTiles = Grid:new()			-- Stores the flipped tile locations. 1 = flipped X, 2 = flipped Y, 3 = both
-	tl._afterTileFunctions = Grid:new()		-- Functions that must happen right after a tile is drawn.
-	tl._tileClipboard = nil					-- The value that is copied and pasted in tileCopy() and tilePaste)
-	tl._previousUseSpriteBatch = false		-- The previous useSpriteBatch. If this is different then we 
-												-- need to force a special redraw
-	return setmetatable(tl, TileLayer)
+   assert( map and name , "TileLayer:new - Needs at least 2 parameters for the map and name.")
+   local tl = {}
+
+   -- Public:
+   tl.name = name		-- The name of the tile layer
+   tl.map = map 		-- The map that this layer belongs to
+   tl.opacity = opacity or 1	-- The opacity to draw the tiles (0-1)
+   tl.properties = prop or {}	-- Properties set by Tiled
+   tl.tileData = Grid:new()	-- A grid containing the tile locations
+   tl.useSpriteBatch = nil	-- If true then the layer is rendered with sprite batches. If
+				-- false then the layer will not use sprite batches. If nil then
+				-- map.useSpriteBatch will be used. Note that using sprite
+				-- batches will break the draw order when using multiple tilesets
+				-- on the same layer. Using Map.drawAfterTile is also not possible.
+   -- Private:
+   tl._batches = {}		-- Keeps track of the sprite batches for each tileset
+   tl._flippedTiles = Grid:new()	-- Stores the flipped tile locations. 1 = flipped X, 2 = flipped Y, 3 = both
+   tl._afterTileFunctions = Grid:new()	-- Functions that must happen right after a tile is drawn.
+   tl._tileClipboard = nil	-- The value that is copied and pasted in tileCopy() and tilePaste)
+   tl._previousUseSpriteBatch = false	-- The previous useSpriteBatch. If this is different then we
+					-- need to force a special redraw
+   return setmetatable(tl, TileLayer)
 end
 
 -- Clears the draw list of any functions
 function TileLayer:clearAfterTile()
-	for x,y,v in self._afterTileFunctions:iterate() do
-		self._afterTileFunctions:set(x,y,nil)
-	end
+   for x,y,v in self._afterTileFunctions:iterate() do
+      self._afterTileFunctions:set(x,y,nil)
+   end
 end
 
 -- Adds a function to the tile's draw list
 function TileLayer:drawAfterTile(x, y, funct)
-	if self.useSpriteBatch ~= nil and self.useSpriteBatch or self.map.useSpriteBatch then 
+	if self.useSpriteBatch ~= nil and self.useSpriteBatch or self.map.useSpriteBatch then
 		error("TileLayer:drawAfterTile - This function is not possible with sprite batches enabled. Self: " .. (self.useSpriteBatch and "true" or "false") .. " Map: " .. (map.useSpriteBatch and "true" or "false") )
 	end
 	self._afterTileFunctions:set(x, y, funct)
@@ -60,38 +60,38 @@ local at, drawX, drawY, flipX, flipY, r, g, b, a, halfW, halfH
 -- Draws the TileLayer.
 function TileLayer:draw()
 
-	-- We access these a lot so we'll shorted them a bit. 
+	-- We access these a lot so we'll shorted them a bit.
 	map, tiles, tileData = self.map, self.map.tiles, self.tileData
 	-- Same with post draw
 	postDraw = self.postDraw
-	
+
 	-- If useSpriteBatch was turned on then we need to force redraw the sprite batches
 	if self.useSpriteBatch ~= self._previousUseSpriteBatch then map:forceRedraw() end
 	-- Set the previous useSpriteBatch
 	self._previousUseSpriteBatch = self.useSpriteBatch
 	-- If useSpriteBatch is set for this layer then use that, otherwise use the map's setting.
 	useSpriteBatch = self.useSpriteBatch ~= nil and self.useSpriteBatch or map.useSpriteBatch
-	
+
 	-- We'll blend the set alpha in with the current alpha
 	r,g,b,a = love.graphics.getColor()
 	love.graphics.setColor(r,g,b, a*self.opacity)
-	
+
 	self._previousUseSpriteBatch = self.useSpriteBatch
-	
-	
+
+
 	-- Clear sprite batches if the screen has changed.
 	if map._specialRedraw and useSpriteBatch then
 		for k,v in pairs(self._batches) do
 			v:clear()
 		end
 	end
-	
+
 	-- Get the tile range
 	local x1, y1, x2, y2 = map._tileRange[1], map._tileRange[2], map._tileRange[3], map._tileRange[4]
-	
+
 	-- Only draw if we're not using sprite batches or we need to update the sprite batches.
 	if map._specialRedraw or not useSpriteBatch then
-	
+
 		-- Orthogonal tiles
 		if map.orientation == "orthogonal" then
 			-- Go through each tile
@@ -111,20 +111,20 @@ function TileLayer:draw()
 				-- If we are using spritebatches
 				if useSpriteBatch then
 					-- If we dont have a spritebatch for the current tile's tileset then make one
-					if not self._batches[tile.tileset] then 
+					if not self._batches[tile.tileset] then
 						self._batches[tile.tileset] = love.graphics.newSpriteBatch(tile.tileset.image, map.width * map.height)
 					end
 					-- Add the quad to the spritebatch
-					self._batches[tile.tileset]:addq(tile.quad, drawX + halfW + (rot and halfW or 0), 
-													drawY-halfH+(rot and halfW or 0), 
-													rot and math.pi*1.5 or 0, 
+					self._batches[tile.tileset]:addq(tile.quad, drawX + halfW + (rot and halfW or 0),
+													drawY-halfH+(rot and halfW or 0),
+													rot and math.pi*1.5 or 0,
 													flipX, flipY, halfW, halfH)
 				-- If we are not using spritebatches
 				else
 					-- Draw the tile
-					tile:draw(drawX + halfW + (rot and halfW or 0), 
-												drawY - halfH + (rot and halfW or 0), 
-												rot and math.pi*1.5 or 0, 
+					tile:draw(drawX + halfW + (rot and halfW or 0),
+												drawY - halfH + (rot and halfW or 0),
+												rot and math.pi*1.5 or 0,
 												flipX, flipY, halfW, halfH)
 					-- If there's something in the _afterTileFunctions for this tile then call it
 					at = self._afterTileFunctions(x,y)
@@ -133,7 +133,7 @@ function TileLayer:draw()
 				end
 			end
 		end
-		
+
 		-- Isometric tiles
 		if map.orientation == "isometric" then
 			local x,y
@@ -141,7 +141,7 @@ function TileLayer:draw()
 			draw_start = map.height * map.tileWidth/2
 			-- Draw each tile starting from the top left tile. Make sure we have enough
 			-- room to draw the widest and tallest tile in the map.
-			for down=0,y2 do 
+			for down=0,y2 do
 				for layer=0,1 do
 					for right=0,x2 do
 						x = x1 + right + down + layer - 1
@@ -160,7 +160,7 @@ function TileLayer:draw()
 							-- Get the tile
 							tile = tileData(x,y)
 							-- If the tile exists then draw the tile
-							if tile then 
+							if tile then
 								-- Get the half-width and half-height
 								halfW, halfH = tile.width*0.5, tile.height*0.5
 								-- Get the tile draw location
@@ -169,21 +169,21 @@ function TileLayer:draw()
 								-- Using sprite batches
 								if useSpriteBatch then
 									-- If we dont have a spritebatch for the current tile's tileset then make one
-									if not self._batches[tile.tileset] then 
+									if not self._batches[tile.tileset] then
 										self._batches[tile.tileset] = love.graphics.newSpriteBatch(
-																				tile.tileset.image, 
+																				tile.tileset.image,
 																				map.width * map.height)
 									end
 									-- Add the tile to the sprite batch.
-									self._batches[tile.tileset]:addq(tile.quad, drawX + halfW + (rot and halfW or 0), 
-																	drawY-halfH+(rot and halfW or 0), 
-																	rot and math.pi*1.5 or 0, 
+									self._batches[tile.tileset]:addq(tile.quad, drawX + halfW + (rot and halfW or 0),
+																	drawY-halfH+(rot and halfW or 0),
+																	rot and math.pi*1.5 or 0,
 																	flipX, flipY, halfW, halfH)
 								-- Not using sprite batches
 								else
-									tile:draw(drawX + halfW + (rot and halfW or 0), 
-												drawY - halfH + (rot and halfW or 0), 
-												rot and math.pi*1.5 or 0, 
+									tile:draw(drawX + halfW + (rot and halfW or 0),
+												drawY - halfH + (rot and halfW or 0),
+												rot and math.pi*1.5 or 0,
 												flipX, flipY, halfW, halfH)
 									-- If there's something in the _afterTileFunctions for this tile then call it
 									at = self._afterTileFunctions(x,y)
@@ -204,10 +204,10 @@ function TileLayer:draw()
 			love.graphics.draw(v)
 		end
 	end
-	
+
 	-- Clears the draw list
 	self:clearAfterTile()
-	
+
 	-- Change the color back
 	love.graphics.setColor(r,g,b,a)
 end
@@ -217,8 +217,8 @@ end
 -- rotation and flipped status. You can copy and paste between layers.
 local flippedVal = 2^29
 function TileLayer:tileCopy(x,y)
-	if not self.tileData(x,y) then 
-		self.map._tileClipboard = 0 
+	if not self.tileData(x,y) then
+		self.map._tileClipboard = 0
 	else
 		self.map._tileClipboard = self.tileData(x,y).id + (self._flippedTiles(x,y) or 0) * flippedVal
 	end
@@ -227,7 +227,7 @@ end
 -- Paste a copied tile.
 function TileLayer:tilePaste(x,y)
 	assert(self.map._tileClipboard, "TileLayer:tilePaste() - A tile must be copied with tileCopy() before pasting")
-	local clip = self.map._tileClipboard 
+	local clip = self.map._tileClipboard
 	if clip / flippedVal > 0 then
 		self._flippedTiles:set(x, y, floor(clip / flippedVal))
 	end
@@ -237,9 +237,9 @@ end
 -- Flip the tile's X. If doFlip is not specified then the flip is toggled.
 function TileLayer:tileFlipX(x, y, doFlip)
 	local flip = self._flippedTiles(x,y) or 0
-	if doFlip ~= false and flip < 4 then 
+	if doFlip ~= false and flip < 4 then
 		flip = flip + 4
-	elseif doFlip ~= true and flip >= 4 then 
+	elseif doFlip ~= true and flip >= 4 then
 		flip = flip - 4
 	end
 	self._flippedTiles:set(x, y, flip ~= 0 and flip or nil)
@@ -248,9 +248,9 @@ end
 -- Flip the tile's Y. If doFlip is not specified then the flip is toggled.
 function TileLayer:tileFlipY(x, y, doFlip)
 	local flip = self._flippedTiles(x,y) or 0
-	if doFlip ~= false and flip % 4 < 2 then 
+	if doFlip ~= false and flip % 4 < 2 then
 		flip = flip + 2
-	elseif doFlip ~= true and flip % 4 >= 2 then 
+	elseif doFlip ~= true and flip % 4 >= 2 then
 		flip = flip - 2
 	end
 	self._flippedTiles:set(x, y, flip ~= 0 and flip or nil)
@@ -261,13 +261,13 @@ function TileLayer:tileRotate(x, y, rot)
 	local flip = self._flippedTiles(x,y) or 0
 	if rot then flip = rot % 8
 	elseif flip == 0 then flip = 5
-	elseif flip == 1 then flip = 4 
+	elseif flip == 1 then flip = 4
 	elseif flip == 2 then flip = 1
-	elseif flip == 3 then flip = 0 
+	elseif flip == 3 then flip = 0
 	elseif flip == 4 then flip = 7
 	elseif flip == 5 then flip = 6
 	elseif flip == 6 then flip = 3
-	elseif flip == 7 then flip = 2 
+	elseif flip == 7 then flip = 2
 	end
 	self._flippedTiles:set(x, y, flip ~= 0 and flip or nil)
 end
@@ -291,30 +291,30 @@ function TileLayer:_populate(t)
 			lasty = math.floor(i/self.map.width)
 		end
 	end
-	
-	
+
+
 	-- Some temporary storage
 	local width, height =  self.map.width, self.map.height
 	local tileID
-	
+
 	-- The values that indicate flipped tiles are in the last three binary digits. We need
 	-- to seperate those.
 	local flipped = 2^29
-	
+
 	-- Create a new grid
 	self.tileData = Grid:new()
-	
+
 	-- Go through every tile
 	for x,y,v in self.tileData:rectangle(0,0,width-1,height-1,true) do
 		tileID = t[width*y+x+1] or 0
-		
+
 		-- If the tile has a value in the last three binary digits then we seperate them
-		if tileID >= flipped then 
+		if tileID >= flipped then
 			--error( string.format( "Tile at (%d,%d) is %g", x, y, floor(tileID / flipped)))
 			self._flippedTiles:set(x, y, floor(tileID / flipped))
 			tileID = tileID % flipped
 		end
-		
+
 		-- Set the tile
 		self.tileData:set(x,y, self.map.tiles[tileID] )
 	end
