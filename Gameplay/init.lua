5d5c18b8c6fc9a3fa046710ae55ee47579d450f4
Luis Cabellos <zhen.sydow@gmail.com>
2012-09-09 12:09:29 +0200

    load AnimManager in Gameplayer

--- Gameplay/init.lua
+++ Gameplay/init.lua
@@ -3,5 +3,6 @@ GAMEPLAY_PATH = GAMEPLAY_PATH or ({...})[1]:gsub("[%.\\/]init$", "") .. '.'
 -- Return the classes in a table
 return {
    World = require(GAMEPLAY_PATH .. "World"),
-   Player = require(GAMEPLAY_PATH  .. "Player")
+   Player = require(GAMEPLAY_PATH  .. "Player"),
+   AnimManager = require(GAMEPLAY_PATH .. "AnimManager")
 }
