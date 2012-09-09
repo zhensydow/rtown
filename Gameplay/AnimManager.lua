f8de4f69989d5a82c7a2c63633c4d6d5255a98bb
Luis Cabellos <zhen.sydow@gmail.com>
2012-09-09 12:09:09 +0200

    add anim manager singleton

---
+++ Gameplay/AnimManager.lua
@@ -0,0 +1,31 @@
+-- -----------------------------------------------------------------------------
+AnimManager = {}
+AnimManager.__index = AnimManager
+
+-- -----------------------------------------------------------------------------
+local TSTEP = 0.1
+AnimManager.t_accum = 0
+AnimManager.entities = {}
+
+-- -----------------------------------------------------------------------------
+function AnimManager.update( dt )
+   local accum = AnimManager.t_accum + dt
+
+   while accum >= TSTEP do
+      accum = accum - TSTEP
+      for i,k in pairs( AnimManager.entities ) do
+	 k:updateAnim()
+      end
+   end
+   AnimManager.t_accum = accum
+end
+
+-- -----------------------------------------------------------------------------
+function AnimManager.addEntity( e )
+   table.insert( AnimManager.entities, e )
+end
+
+-- -----------------------------------------------------------------------------
+return AnimManager
+
+-- -----------------------------------------------------------------------------
