3c60176cb865bda46f47dc2fc3b5c2a2dfcc39ad
Luis Cabellos <zhen.sydow@gmail.com>
2012-09-20 20:01:31 +0200

    remove unused modules

--- conf.lua
+++ conf.lua
@@ -2,6 +2,8 @@
 function love.conf( t )
    t.title = "Robot Town"
    t.author = "Luis Cabellos"
+   t.modules.physics = false
+   t.modules.joystick= false
 end
 
 -- -----------------------------------------------------------------------------
